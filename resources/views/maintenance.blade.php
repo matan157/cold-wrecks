01fa081ec064be924d8d53f6e36e0d834970f55c
Matan Uchen <matan.uchen@gmail.com>
2017-10-05 23:26:34 -0400

    Added music video for maintenance page.

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -5,6 +5,7 @@
 @section('content')
   <div id="maintenance">
     <img id="maintenance-logo" src="images/cw_logo.png" alt="logo" />
-    <h2>We're working on a new site right now. See you soon!</h2>
+    <p>We're working on a new site right now. Check out the video for our new single, "Therapy":</p>
+    <iframe width="640" height="360" src="https://www.youtube.com/embed/-pH6bRPD1Ug" frameborder="0" allowfullscreen></iframe>
   </div>
 @endsection
