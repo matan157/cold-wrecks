5a5b8a8bb942abbbfa0852ddba9a980cd9075fa1
Matan Uchen <matan.uchen@gmail.com>
2017-10-05 23:13:38 -0400

    Added basic maintenance page.

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'Maintenance')
+
+@section('content')
+  <div id="maintenance">
+    <img id="maintenance-logo" src="images/cw_logo.png" alt="logo" />
+    <h2>We're working on a new site right now. See you soon!</h2>
+  </div>
+@endsection
